c99ca699800020f3317430e2cf423564fb83318f
Sachin Gupta <saching@microsoft.com>
2019-05-13 19:19:15 -0700

    adding js files

---
+++ lib/bot.d.ts
@@ -0,0 +1,24 @@
+import { TurnContext, BotState } from 'botbuilder';
+export declare class TeamsBot {
+    private readonly countAccessor;
+    private readonly conversationState;
+    private readonly activityProc;
+    /**
+     *
+     * @param {ConversationState} conversation state object
+     */
+    constructor(conversationState: BotState);
+    /**
+     * Use onTurn to handle an incoming activity, received from a user, process it, and reply as needed
+     *
+     * @param {TurnContext} context on turn context object.
+     */
+    onTurn(turnContext: TurnContext): Promise<void>;
+    /**
+     *  Set up all activity handlers
+     */
+    private setupHandlers;
+    private sendCards;
+    private taskModuleResponse;
+    private taskModuleResponseCard;
+}
