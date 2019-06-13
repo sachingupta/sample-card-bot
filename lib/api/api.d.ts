68ce4ab5d8b57672a3afe1e078c26ff5a68b08e4
Sachin Gupta <saching@microsoft.com>
2019-06-13 12:38:27 -0700

    updating adaptive cards

--- lib/api/api.d.ts
+++ lib/api/api.d.ts
@@ -1,8 +1,9 @@
 import { Attachment } from 'botbuilder';
 import * as teams from 'botbuilder-teams';
 export declare const handleQuery: (searchtext: string) => any[];
-export declare const createPreviewList: (items: any[], heroCard: any) => any[];
+export declare const createPreviewList: (items: any[], heroCard: any, heroCard2: any) => any[];
 export declare const getCustomAdaptiveCard: (body: string) => Attachment;
+export declare const getCustomAdaptiveCard2: (body: string) => Attachment;
 export declare const getAdaptiveCard: () => Attachment;
 export declare const taskModuleResponse: (query: any, done: boolean) => teams.TaskModuleResponseBase;
 export declare const taskModuleResponseCard: (data: any, textValue?: string) => Attachment;
--- src/api/api.ts
+++ src/api/api.ts
@@ -5,10 +5,11 @@ import { adaptiveCardBody } from './adaptivecard';
 
 // Function to handle query fomr bot and output a list of desired items as adaptive cards
 export const handleQuery = (searchtext: string) => {
-    const heroCard = getCustomAdaptiveCard(adaptiveCardBody);
+    const heroCard1 = getCustomAdaptiveCard(adaptiveCardBody);
+    const heroCard2 = getCustomAdaptiveCard2(adaptiveCardBody);
     // Writing 'all' in the search bar will display all cards stored
     if (!searchtext || searchtext.toLowerCase() === 'all') {
-        return (createPreviewList(data, heroCard))
+        return (createPreviewList(data, heroCard1, heroCard2))
     }
     // Writing anything else in the search bar will filter the displayed cards
     else {
@@ -18,91 +19,196 @@ export const handleQuery = (searchtext: string) => {
                 queriedItems.push(item);
             }
         })
-        return (createPreviewList(queriedItems, heroCard))
+        return (createPreviewList(queriedItems, heroCard1, heroCard2))
     }
 }
 
 // Function to process a list of items into a list of cards for output
-export const createPreviewList = (items: Array<any>, heroCard: any) => {
-    let out = items.map((item: any) => {
-        return ({
-            ...heroCard,
-            preview: CardFactory.thumbnailCard(item.title, item.subTitle, [item.heroImageSrc]),
-        })
+export const createPreviewList = (items: Array<any>, heroCard: any, heroCard2: any) => {
+    let out = items.map((item: any, index: number) => {
+        if (index % 2) {
+            return ({
+                ...heroCard,
+                preview: CardFactory.thumbnailCard(item.title, item.subTitle, [item.heroImageSrc]),
+            })
+        }
+        else {
+            return ({
+                ...heroCard2,
+                preview: CardFactory.thumbnailCard(item.title, item.subTitle, [item.heroImageSrc]),
+            })
+        }
     })
     return out;
 }
 
 export const getCustomAdaptiveCard = (body: string) => {
+    const actions1 = [
+        teams.TeamsFactory.adaptiveCardAction({
+            type: ActionTypes.ImBack,
+            title: 'imBack',
+            value: 'text'
+        }),
+
+        teams.TeamsFactory.adaptiveCardAction({
+            type: 'invoke',
+            title: 'invoke',
+            value: { key: 'value' }
+        }),
+        {
+            "type": "Action.OpenUrl",
+            "title": "OpenUrl",
+            "url": "http://adaptivecards.io"
+        },
+        {
+            "type": "Action.Submit",
+            "title": "Action.Submit",
+            "data": {
+                "x": 13
+            }
+        },
+        {
+            "type": "Action.ShowCard",
+            "title": "ShowCard (Set due date)",
+            "card": {
+                "type": "AdaptiveCard",
+                "body": [
+                    {
+                        "type": "Input.Date",
+                        "id": "dueDate"
+                    }
+                ],
+                "actions": [
+                    {
+                        "type": "Action.Submit",
+                        "title": "OK"
+                    }
+                ]
+            }
+        },
+        {
+            "type": "Action.Submit",
+            "title": "signin",
+            "data": {
+                "msteams": {
+                    "type": "signin",
+                    "value": "https://signin.com"
+                }
+            }
+        }
+    ];
+
+    const action2 = [
+        teams.TeamsFactory.adaptiveCardAction({
+            type: ActionTypes.ImBack,
+            title: 'imBack',
+            value: 'text'
+        }),
+        teams.TeamsFactory.adaptiveCardAction({
+            type: ActionTypes.MessageBack,
+            title: 'message back',
+            value: { key: 'value' },
+            text: 'text received by bots',
+            displayText: 'text display to users',
+        }),
+        teams.TeamsFactory.adaptiveCardAction({
+            type: 'invoke',
+            title: 'invoke',
+            value: { key: 'value' }
+        }),
+        {
+            "type": "Action.OpenUrl",
+            "title": "OpenUrl",
+            "url": "http://adaptivecards.io"
+        },
+        {
+            "type": "Action.OpenUrl",
+            "title": "JSON Tab",
+            "url": 'https://teams.microsoft.com/l/task/300639bf-2c0f-41a7-aa2e-7833664c4c76?&title=First%20Page&url=https%3A%2F%2Fteams-json-tab.azurewebsites.net%2F%3Ftheme%3D%257Btheme%257D%26frameContext%3Dcontent'
+        }
+    ];
+
     let adaptiveCard = teams.TeamsFactory.adaptiveCard({
         version: '1.0.0',
         type: 'AdaptiveCard',
         body: JSON.parse(body),
-        actions: [
-            teams.TeamsFactory.adaptiveCardAction({
-                type: ActionTypes.ImBack,
-                title: 'imBack',
-                value: 'text'
-            }),
-            teams.TeamsFactory.adaptiveCardAction({
-                type: ActionTypes.MessageBack,
-                title: 'message back',
-                value: { key: 'value' },
-                text: 'text received by bots',
-                displayText: 'text display to users',
-            }),
-            teams.TeamsFactory.adaptiveCardAction({
-                type: 'invoke',
-                title: 'invoke',
-                value: { key: 'value' }
-            }),
-            {
-                "type": "Action.OpenUrl",
-                "title": "OpenUrl",
-                "url": "http://adaptivecards.io"
-            },
-            {
-                "type": "Action.Submit",
-                "title": "Action.Submit",
-                "data": {
-                    "x": 13
-                }
-            },
-            {
-                "type": "Action.ShowCard",
-                "title": "ShowCard (Set due date)",
-                "card": {
-                    "type": "AdaptiveCard",
-                    "body": [
-                        {
-                            "type": "Input.Date",
-                            "id": "dueDate"
-                        }
-                    ],
-                    "actions": [
-                        {
-                            "type": "Action.Submit",
-                            "title": "OK"
-                        }
-                    ]
-                }
-            },
-            {
-                "type": "Action.Submit",
-                "title": "signin",
-                "data": {
-                    "msteams": {
-                        "type": "signin",
-                        "value": "https://signin.com"
+        actions: actions1 as any
+    });
+    return adaptiveCard;
+}
+
+export const getCustomAdaptiveCard2 = (body: string) => {
+    const action2 = [
+        teams.TeamsFactory.adaptiveCardAction({
+            type: ActionTypes.ImBack,
+            title: 'imBack',
+            value: 'text'
+        }),
+        teams.TeamsFactory.adaptiveCardAction({
+            type: ActionTypes.MessageBack,
+            title: 'message back',
+            value: { key: 'value' },
+            text: 'text received by bots',
+            displayText: 'text display to users',
+        }),
+        teams.TeamsFactory.adaptiveCardAction({
+            type: 'invoke',
+            title: 'invoke',
+            value: { key: 'value' }
+        }),
+        {
+            "type": "Action.OpenUrl",
+            "title": "OpenUrl",
+            "url": "http://adaptivecards.io"
+        },
+        {
+            "type": "Action.Submit",
+            "title": "Action.Submit",
+            "data": {
+                "x": 13
+            }
+        },
+        {
+            "type": "Action.ShowCard",
+            "title": "ShowCard (Set due date)",
+            "card": {
+                "type": "AdaptiveCard",
+                "body": [
+                    {
+                        "type": "Input.Date",
+                        "id": "dueDate"
                     }
+                ],
+                "actions": [
+                    {
+                        "type": "Action.Submit",
+                        "title": "OK"
+                    }
+                ]
+            }
+        },
+        {
+            "type": "Action.Submit",
+            "title": "signin",
+            "data": {
+                "msteams": {
+                    "type": "signin",
+                    "value": "https://signin.com"
                 }
-            },
-            {
-                "type": "Action.OpenUrl",
-                "title": "JSON Tab",
-                "url": 'https://teams.microsoft.com/l/task/300639bf-2c0f-41a7-aa2e-7833664c4c76?&title=First%20Page&url=https%3A%2F%2Fteams-json-tab.azurewebsites.net%2F%3Ftheme%3D%257Btheme%257D%26frameContext%3Dcontent'
-            } as any
-        ]
+            }
+        },
+        {
+            "type": "Action.OpenUrl",
+            "title": "JSON Tab",
+            "url": 'https://teams.microsoft.com/l/task/300639bf-2c0f-41a7-aa2e-7833664c4c76?&title=First%20Page&url=https%3A%2F%2Fteams-json-tab.azurewebsites.net%2F%3Ftheme%3D%257Btheme%257D%26frameContext%3Dcontent'
+        }
+    ];
+
+    let adaptiveCard = teams.TeamsFactory.adaptiveCard({
+        version: '1.0.0',
+        type: 'AdaptiveCard',
+        body: JSON.parse(body),
+        actions: action2 as any
     });
     return adaptiveCard;
 }
